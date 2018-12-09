6358608ebfe1b5a06011762dc718cbb4e18f8e59
Artem Konopelko <artemkonopelko@gmail.com>
2018-12-09 03:08:01 +0300

    Remove statement that destroy menu

--- cyclic-menu/src/index.js
+++ cyclic-menu/src/index.js
@@ -4,8 +4,6 @@ import links from "./menuLinks.js";
 
 const ready = (e) => {
     const cyclMenu = new CyclicMenu(links,config, document.body);
-
-    cyclMenu.removeMenu();
 }
 
 document.addEventListener("DOMContentLoaded", ready);
