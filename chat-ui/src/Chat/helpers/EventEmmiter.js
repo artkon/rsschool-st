fde0e2d438f5179cd57ae13cea13e14bc3fcf089
Artem Konopelko <artemkonopelko@gmail.com>
2019-01-23 16:43:26 +0300

    Add unsubscribe method

--- chat-ui/src/Chat/helpers/EventEmmiter.js
+++ chat-ui/src/Chat/helpers/EventEmmiter.js
@@ -6,12 +6,20 @@ class EventEmmiter {
     on(type, callback) {
         this.events[type] = this.events[type] || [];
         this.events[type].push(callback);
+        const index = this.events[type].length - 1;
+        return function () {
+            this.unsubscribe(type, index).bind(this);
+        }
     }
 
     emit(type, arg) {
         if (this.events[type]) {
             this.events[type].forEach( callback => callback(arg) );
         }
+    }
+    
+    unsubscribe(type, index) {
+        this.events[type].splice(index, 1);
     }
 }
 
